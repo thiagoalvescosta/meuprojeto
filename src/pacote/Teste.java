7282f35d8b0c124f2c797d1473f610abfcd14c5f
Thiago Costa <thiagoalvescosta@gmail.com>
2015-04-27 09:17:49 -0300

    Primeiro Commit

---
+++ src/pacote/Teste.java
@@ -0,0 +1,21 @@
+package pacote;
+
+
+/**
+ * Classe que representa ...
+ * 
+ * @author Usuário de Teste
+ * @version 1.0
+ * @since 2015-04-27
+ *
+ */
+ 
+public class Teste {
+
+	/**
+	 * Construtor
+	 **/
+	public Teste (){
+	}
+
+}
