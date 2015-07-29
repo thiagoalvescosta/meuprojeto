01e5db84593c28b439d7aa24998a1d3eba05ffec
Fábio Costa <fcostasilva@gmail.com>
2015-07-29 19:29:58 -0300

    [asdads.java] added to version control.

---
+++ src/pacote/asdads.java
@@ -0,0 +1,21 @@
+package pacote;
+
+
+/**
+ * Classe que representa ...
+ * 
+ * @author Usuário de Teste
+ * @version 1.0
+ * @since 2015-07-29
+ *
+ */
+ 
+public class asdads {
+
+	/**
+	 * Construtor
+	 **/
+	public asdads (){
+	}
+
+}
