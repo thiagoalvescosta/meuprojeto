e8d82f6f2a9f6bba9e1e8b5b8592e904683c98b7
Fábio Costa <fcostasilva@gmail.com>
2015-07-29 19:04:33 -0300

    Último commit

---
+++ src/pacote/Day.java
@@ -0,0 +1,16 @@
+package pacote;
+
+
+/**
+ * Enumeração que representa ...
+ * 
+ * @author Usuário de Teste
+ * @version 1.0
+ * @since 2015-07-29
+ *
+ */
+ 
+public enum Day {
+    SUNDAY, MONDAY, TUESDAY, WEDNESDAY,
+    THURSDAY, FRIDAY, SATURDAY 
+}
