58fee4a8f5a835e24dfa8550cf8fb75d4210d294
Fábio Costa <fcostasilva@gmail.com>
2015-07-29 19:40:07 -0300

    asd

---
+++ src/pacote/1qazxsw2.java
@@ -0,0 +1,21 @@
+package pacote;
+
+
+/**
+ * Classe que representa ...
+ * 
+ * @author Usuário de Teste
+ * @version 1.0
+ * @since 2015-07-29
+ *
+ */
+ 
+public class 1qazxsw2 {
+
+	/**
+	 * Construtor
+	 **/
+	public 1qazxsw2 (){
+	}
+
+}
