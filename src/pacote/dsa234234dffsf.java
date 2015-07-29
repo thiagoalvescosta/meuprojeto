3fd23d75bf43c087f650b0341abe083cc7f4ba6b
Fábio Costa <fcostasilva@gmail.com>
2015-07-29 17:55:44 -0300

    sdf

---
+++ src/pacote/dsa234234dffsf.java
@@ -0,0 +1,21 @@
+package pacote;
+
+
+/**
+ * Classe que representa ...
+ * 
+ * @author Usuário de Teste
+ * @version 1.0
+ * @since 2015-07-29
+ *
+ */
+ 
+public class dsa234234dffsf {
+
+	/**
+	 * Construtor
+	 **/
+	public dsa234234dffsf (){
+	}
+
+}
